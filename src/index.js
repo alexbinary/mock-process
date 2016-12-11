413307654b5990d04720c80b6c9f9a54331acc06
Alexandre Bintz <alexandre@bintz.io>
2016-12-11 11:52:27 +0100

    Add comments

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ let originalProperties = {}
 
 function mock () {
   if (typeof arguments[0] === 'string') {
+    // mock(name, value)
     mockProperty(arguments[0], arguments[1])
   } else {
+    // mock({name: value, ...})
     for (let name in arguments[0]) {
       mockProperty(name, arguments[0][name])
     }
@@ -18,11 +20,14 @@ function mockProperty (name, value) {
 
 function restore () {
   if (arguments.length === 0) {
+    // restore() -> restore all properties
     restoreAll()
   } else {
     if (typeof arguments[0] === 'string') {
+      // restore(name) -> restore process.name
       restoreProperty(arguments[0])
     } else {
+      // restore([name1, name2, ...])
       restoreProperties(arguments[0])
     }
   }
