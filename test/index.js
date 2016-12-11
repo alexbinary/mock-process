6e115a7315e6e65319a0f642bfc99e8563326e8e
Alexandre Bintz <alexandre@bintz.io>
2016-12-11 11:43:04 +0100

    [test] add annotations

--- test/index.js
+++ test/index.js
@@ -12,44 +12,62 @@ describe('mock process', function () {
   })
   describe('#mock(name, value)', function () {
     it('replace property of given name with given value', function () {
+      // ## TEST
       mockProcess.mock('platform', 'mockOS')
+      // ## Assert
       expect(process.platform).to.equal('mockOS')
+      // ## End
     })
   })
   describe('#mock(properties)', function () {
     it('replace several properties described in an object', function () {
+      // ## TEST
       mockProcess.mock({platform: 'mockOS'})
+      // ## Assert
       expect(process.platform).to.equal('mockOS')
+      // ## End
     })
   })
   describe('#restore(name)', function () {
     it('restore original property of given name', function () {
+      // ## Setup
       let originalPlatform = process.platform
       mockProcess.mock('platform', 'mockOS')
+      // ## TEST
       mockProcess.restore('platform')
+      // ## Assert
       expect(process.platform).to.equal(originalPlatform)
+      // ## End
     })
   })
   describe('#restore(names)', function () {
     it('restore listed properties', function () {
+      // ## Setup
       let originalPlatform = process.platform
       let originalRelease = process.release
       mockProcess.mock('platform', 'mockOS')
       mockProcess.mock('release', 'mockRelease')
+      // ## TEST
       mockProcess.restore(['platform', 'release'])
+      // ## Assert
       expect(process.platform).to.equal(originalPlatform)
       expect(process.release).to.equal(originalRelease)
+      // ## End
     })
   })
   describe('#restore()', function () {
     it('restore all mocked properties', function () {
+      // ## Setup
       let originalPlatform = process.platform
       let originalRelease = process.release
       mockProcess.mock('platform', 'mockOS')
       mockProcess.mock('release', 'mockRelease')
+      // ## TEST
       mockProcess.restore()
+      // ## Assert
       expect(process.platform).to.equal(originalPlatform)
       expect(process.release).to.equal(originalRelease)
+      // ## End
     })
   })
 })
